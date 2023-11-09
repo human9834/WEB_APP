016ac7852de3705186a2f10f24c9bb71c5c42c02
human9834 <hcaos83n@gmail.com>
2023-11-09 15:54:34 +0900

    変更

--- app.py
+++ app.py
@@ -50,10 +50,10 @@ with st.form(key='profile_form'):
 
 
 # データ分析
-# df = pd.read_excel('sanada.xlsx', sheet_name=None)
+df = pd.read_excel('sanada.xlsx', sheet_name=None)
 # st.bar_chart(df['202301']) 棒グラフの表示
 # st.line_chart(df) #チャート表示され利
-# st.dataframe(df) # データを表示
+st.dataframe(df) # データを表示
 # st.table(df) # データが画面全体で表示される
     
 
